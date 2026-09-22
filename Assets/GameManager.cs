9d3145433851eee5b2bf7d5a9ec8a8518e2db98a
zyh625 <2578687806@qq.com>
2026-09-22 17:51:41 +0800

    更新角色重力系统，实现跳跃

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,20 +10,27 @@ public class AnimationData
 public class GameManager : MonoBehaviour
 {
     public AnimationData[] monkeys;//角色的所有动作的帧动画
-    public Canvas canvas;
-    public GameObject monkeyPrefab;
-    private Transform tran;
-    public int act = 0;//0表示静止，1表示行走，2表示跑步，3表示跳跃，4表示攻击
-    public float left, right;
+    public GameObject monkeyPrefab;//预先创建好的角色实例
+
+    private Transform tran;//角色位置
+    private SpriteRenderer sr;//角色精灵
+    public Rigidbody2D rb;//角色的重力系统
+
+    public int act = 0;//0表示静止，1表示行走，2表示跑步，3表示普通跳跃，4表示二连跳跃，5表示攻击
     public float moveRate = 10;
     public int curFrame = 0;//当前帧序列
     public float delayTime = 0;
     public bool face = true;//角色朝向，开始向右
+    public float left, right, top, bottom;//游戏边界
     void Start()
     {
         tran = monkeyPrefab.transform;
-        left = monkeyPrefab.GetComponent<RectTransform>().rect.width / 2;
-        right = canvas.GetComponent<RectTransform>().rect.width - left;
+        sr = monkeyPrefab.GetComponent<SpriteRenderer>();
+        rb = monkeyPrefab.GetComponent<Rigidbody2D>();
+        float halfSr = sr.bounds.size.x / 2;
+        float halfCm = Camera.main.orthographicSize * Camera.main.aspect;
+        left = halfSr - halfCm;
+        right = -left;
     }
     void Update()
     {
@@ -31,26 +38,28 @@ public class GameManager : MonoBehaviour
     }
     public void Move(bool r,float speed)//角色水平移动
     {
-        if (r ^ face)
-            monkeyPrefab.GetComponent<Image>().rectTransform.localScale = new Vector3(r ? 1 : -1, 1, 1);//面朝移动方向
-        face = r;
+        if (r != face)
+        {
+            sr.flipX = !r;
+            face = r;
+        }
         Vector3 pos = tran.position;
         pos.x += (r ? 1 : -1) * speed;
-        pos.x = Mathf.Max(Mathf.Min(pos.x, right), left);
+        pos.x = Mathf.Clamp(pos.x, left, right);
         tran.position = pos;
     }
     public void Jump()
     {
-
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 10f);
     }
     public void UpdateMonkey()
     {
-        if (act != 0) return;
+        if (act >2||act==0) return;
         delayTime += Time.deltaTime;
         if (delayTime >= 0.5)
         {
             delayTime = 0;
-            monkeyPrefab.GetComponent<Image>().sprite = monkeys[act].frames[curFrame];
+            sr.sprite = monkeys[act].frames[curFrame];
             curFrame = (curFrame + 1) % monkeys[act].frames.Length;
         }
         
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
     float doublePressTime = 0.3f;
     Vector2 input;//检测是否按下A、D键
     Vector2 priInput;//用于判断上次按下的按键是否和这次一样
-    float speed = 0.1f;
+    float speed = 0.003f;
+    int jumped = 0;//检测跳跃次数
     void Update()
     {
         if (input.x != 0)
@@ -21,26 +22,38 @@ public class Player : MonoBehaviour
         input = ctx.ReadValue<Vector2>();
         if (ctx.started)
         {
-            GameManager.act = 1;
+            InitMonkey(1);
             if (lastPressTime!=-1&&Time.time - lastPressTime <= doublePressTime&&priInput.x==input.x)
             {
-                GameManager.act = 2;
-                speed = 0.2f;
+                InitMonkey(2);
+                speed = 0.005f;
             }
             lastPressTime = Time.time;
             priInput = input;
         }
         if (ctx.canceled)
         {
-            GameManager.act = 0;
-            speed = 0.1f;
+            InitMonkey(0);
+            speed = 0.003f;
         }
     }
     public void OnJump(InputAction.CallbackContext ctx)
     {
+        if (GameManager.rb.linearVelocity.y ==0)
+        {
+            jumped = 0;
+        }
         if (ctx.performed)
         {
+            if (jumped == 2) return;
             GameManager.Jump();
+            InitMonkey(3 + jumped);
+            jumped++;
         }
+    }
+    public void InitMonkey(int k)
+    {
+        GameManager.act = k;
+        GameManager.curFrame = 0;
     }
 }
